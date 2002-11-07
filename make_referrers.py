84184b7625a892670a51ae9e8b414d5d3c8e3e65
Phillip Pearson <pp@myelin.co.nz>
2002-11-07 21:04:17 +0000

    put urlparse import back in; pychecker was wrong.

--- make_referrers.py
+++ make_referrers.py
@@ -25,7 +25,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import re, time, sys, ConfigParser, StringIO, os.path
+import re, time, sys, ConfigParser, urlparse, StringIO, os.path
 
 """Log analysis
 
