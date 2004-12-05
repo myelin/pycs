0502a9cf7cca4b9acbb807dc376915871c24f4df
Phillip Pearson <pp@myelin.co.nz>
2004-12-05 11:20:53 +0000

    made it runnable out of the source distro

--- pycs_paths.py
+++ pycs_paths.py
@@ -9,13 +9,32 @@
 # This is set automatically by the install Makefile
 ROOTDIR = '{{PREFIX}}'
 
-import os.path
+import os, os.path
+
+def first_of(*files):
+	"look for the first file in a list that exists"
+	for f in files:
+		if os.path.exists(f):
+			return f
+	return files[0]
+
+# see if we're running out of the source distribution
+if ROOTDIR == '{{PRE' + 'FIX}}':
+	ROOTDIR = os.path.abspath('root')
+	for x in ("", "var", "var/log", "var/log/pycs", "var/run", "var/run/pycs", "var/lib", "var/lib/pycs", "var/lib/pycs/data", "var/lib/pycs/www"):
+		d = os.path.join(ROOTDIR, x)
+		if not os.path.exists(d):
+			os.mkdir(d)
 
 # Config files
 CONFDIR = os.path.join( ROOTDIR, 'etc/pycs' )
+if not os.path.isdir(CONFDIR): CONFDIR=os.path.normpath('.')
+PYCS_CONF = first_of(os.path.join( CONFDIR, "pycs.conf" ), os.path.join(CONFDIR, 'pycs.conf.default'))
+REWRITE_CONF = first_of(os.path.join(CONFDIR, "rewrite.conf"), os.path.join(CONFDIR, "rewrite.conf.default"))
 
 # Read-only stuff
 RODIR = os.path.join( ROOTDIR, 'usr/lib/pycs' )
+if not os.path.isdir(RODIR): RODIR='.'
 
 # Logging
 LOGDIR = os.path.join( ROOTDIR, 'var/log/pycs' )
@@ -36,6 +55,7 @@ WEBDIR = os.path.join( VARDIR, 'www' )
 
 # Scripts
 MODDIR = os.path.join( VARDIR, 'modules' )
+if not os.path.exists(MODDIR): MODDIR=os.path.normpath('./modules')
 
 if __name__ == '__main__':
 	# we are being called by make during installation to replace the prefix with
